128a219568e4b685c2a5507a1c9dcb5e560efa07
EdgarMarker <emendez.marker@gmail.com>
2026-09-04 16:52:50 -0600

    Refactor MetaPixel for better tracking logic
    Refactor MetaPixel component to improve page view tracking and remove unused search parameters.

--- frontend/src/common/utils/MetaPixel.tsx
+++ frontend/src/common/utils/MetaPixel.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Script from "next/script";
+
+type Fbq = {
+	(...args: unknown[]): void;
+	callMethod?: (...args: unknown[]) => void;
+	queue: unknown[][];
+	push: Fbq;
+	loaded: boolean;
+	version: string;
+};
 
 declare global {
 	interface Window {
-		fbq?: (...args: any[]) => void;
-		_fbq?: unknown;
+		fbq?: Fbq;
+		_fbq?: Fbq;
 	}
 }
 
@@ -17,23 +26,20 @@ interface MetaPixelProps {
 
 export default function MetaPixel({ pixelId }: MetaPixelProps) {
 	const pathname = usePathname();
-	const searchParams = useSearchParams();
-
-	const initialized = useRef(false);
-	const firstPageViewTracked = useRef(false);
+	const firstPageView = useRef(true);
 
 	useEffect(() => {
-		if (!pixelId || !window.fbq) return;
+		if (!pixelId) return;
 
-		// El primer PageView ya se dispara al inicializar el Pixel.
-		if (!firstPageViewTracked.current) {
-			firstPageViewTracked.current = true;
+		// El primer PageView ya se dispara al cargar el script.
+		if (firstPageView.current) {
+			firstPageView.current = false;
 			return;
 		}
 
-		// PageView para navegaciones internas de Next.js
-		window.fbq("track", "PageView");
-	}, [pathname, searchParams, pixelId]);
+		// PageView para navegaciones internas de Next.js.
+		window.fbq?.("track", "PageView");
+	}, [pathname, pixelId]);
 
 	if (!pixelId) {
 		return null;
@@ -41,63 +47,21 @@ export default function MetaPixel({ pixelId }: MetaPixelProps) {
 
 	return (
 		<>
-			<Script
-				id="meta-pixel"
-				strategy="afterInteractive"
-				onReady={() => {
-					if (initialized.current || window.fbq) {
-						return;
-					}
+			<Script id="meta-pixel" strategy="afterInteractive">
+				{`
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
 
-					(function (
-						f: Window,
-						b: Document,
-						e: string,
-						v: string,
-						n?: any,
-						t?: HTMLScriptElement,
-						s?: HTMLScriptElement
-					) {
-						if (f.fbq) return;
-
-						n = f.fbq = function (...args: any[]) {
-							if (n.callMethod) {
-								n.callMethod.apply(n, args);
-							} else {
-								n.queue.push(args);
-							}
-						};
-
-						if (!f._fbq) {
-							f._fbq = n;
-						}
-
-						n.push = n;
-						n.loaded = true;
-						n.version = "2.0";
-						n.queue = [];
-
-						t = b.createElement(e) as HTMLScriptElement;
-						t.async = true;
-						t.src = v;
-
-						s = b.getElementsByTagName(e)[0] as HTMLScriptElement;
-
-						s.parentNode?.insertBefore(t, s);
-					})(
-						window,
-						document,
-						"script",
-						"https://connect.facebook.net/en_US/fbevents.js"
-					);
-
-					window.fbq?.("init", pixelId);
-					window.fbq?.("track", "PageView");
-
-					initialized.current = true;
-					firstPageViewTracked.current = true;
-				}}
-			/>
+					fbq('init', '${pixelId}');
+					fbq('track', 'PageView');
+				`}
+			</Script>
 
 			<noscript>
 				<img
